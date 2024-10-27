f37e24ca7bee6667b854e037de5e91a88d5acba6
Marvin Balbutin <balbutinmarvin@gmail.com>
2024-10-28 00:22:02 +0800

    Add Routes.

--- main.tf
+++ main.tf
@@ -3,3 +3,35 @@ resource "aws_route_table" "rtb" {
 
   tags = var.rtb_tags
 }
+
+
+resource "aws_route" "route_internet_gateway" {
+  count                  = var.route_internet_gateway_bool ? 1 : 0
+  route_table_id         = aws_route_table.rtb.id
+  destination_cidr_block = var.route_destination_cidr_block
+  gateway_id             = var.route_internet_gateway
+}
+
+resource "aws_route" "route_nat_gateway" {
+  count                  = var.route_nat_gateway_bool ? 1 : 0
+  route_table_id         = aws_route_table.rtb.id
+  destination_cidr_block = var.route_destination_cidr_block
+  gateway_id             = var.route_nat_gateway
+}
+
+resource "aws_route" "route_endpoint" {
+  count                  = var.route_endpoint_bool ? 1 : 0
+  route_table_id         = aws_route_table.rtb.id
+  destination_cidr_block = var.route_destination_cidr_block
+  gateway_id             = var.route_endpoint
+}
+
+resource "aws_route" "route_peering" {
+  count                  = var.route_peering_bool ? 1 : 0
+  route_table_id         = aws_route_table.rtb.id
+  destination_cidr_block = var.route_destination_cidr_block
+  gateway_id             = var.route_peering
+}
+
+
+
--- variables.tf
+++ variables.tf
@@ -7,3 +7,49 @@ variable "rtb_tags" {
   description = "The tags for this route table."
   type        = map(string)
 }
+
+variable "route_destination_cidr_block" {
+  description = "The destination CIDR block of this route."
+  type        = string
+}
+
+variable "route_internet_gateway_bool" {
+  description = "Create route to Internet Gateway."
+  type        = bool
+}
+
+variable "route_internet_gateway" {
+  description = "The Internet Gateway ID."
+  type        = string
+}
+
+
+variable "route_nat_gateway_bool" {
+  description = "Create route to NAT Gateway."
+  type        = bool
+}
+
+variable "route_nat_gateway" {
+  description = "The NAT Gateway ID."
+  type        = string
+}
+
+variable "route_endpoint_bool" {
+  description = "Create route to VPC Endpoint."
+  type        = bool
+}
+
+variable "route_endpoint" {
+  description = "The VPC Endpoint ID."
+  type        = string
+}
+
+variable "route_peering_bool" {
+  description = "Create route to VPC Peering."
+  type        = bool
+}
+
+variable "route_peering" {
+  description = "The VPC Peering ID."
+  type        = string
+}
