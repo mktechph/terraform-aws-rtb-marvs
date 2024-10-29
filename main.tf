27bb976aa8cf50e8c3bb5420a14a0a696f459c2d
Marvin Balbutin <balbutinmarvin@gmail.com>
2024-10-29 11:47:34 +0800

    Add variables.

--- main.tf
+++ main.tf
@@ -8,28 +8,28 @@ resource "aws_route_table" "rtb" {
 resource "aws_route" "route_internet_gateway" {
   count                  = var.route_internet_gateway_bool ? 1 : 0
   route_table_id         = aws_route_table.rtb.id
-  destination_cidr_block = var.route_destination_cidr_block
+  destination_cidr_block = var.route_internet_gateway_destination_cidr_block
   gateway_id             = var.route_internet_gateway
 }
 
 resource "aws_route" "route_nat_gateway" {
   count                  = var.route_nat_gateway_bool ? 1 : 0
   route_table_id         = aws_route_table.rtb.id
-  destination_cidr_block = var.route_destination_cidr_block
+  destination_cidr_block = var.route_nat_gateway_destination_cidr_block
   gateway_id             = var.route_nat_gateway
 }
 
 resource "aws_route" "route_endpoint" {
   count                  = var.route_endpoint_bool ? 1 : 0
   route_table_id         = aws_route_table.rtb.id
-  destination_cidr_block = var.route_destination_cidr_block
+  destination_cidr_block = var.route_endpoint_destination_cidr_block
   gateway_id             = var.route_endpoint
 }
 
 resource "aws_route" "route_peering" {
   count                  = var.route_peering_bool ? 1 : 0
   route_table_id         = aws_route_table.rtb.id
-  destination_cidr_block = var.route_destination_cidr_block
+  destination_cidr_block = var.route_vpc_peering_destination_cidr_block
   gateway_id             = var.route_peering
 }
 
--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "rtb_tags" {
   type        = map(string)
 }
 
-variable "route_destination_cidr_block" {
-  description = "The destination CIDR block of this route."
-  type        = string
-}
-
 variable "route_internet_gateway_bool" {
   description = "Create route to Internet Gateway."
   type        = bool
@@ -20,7 +15,13 @@ variable "route_internet_gateway_bool" {
 }
 
 variable "route_internet_gateway" {
-  description = "The Internet Gateway ID."
+  description = "The Internet Gateway ID. (Use only if route_internet_gateway_bool is TRUE.)"
+  type        = string
+  default     = null
+}
+
+variable "route_internet_gateway_destination_cidr_block" {
+  description = "The destination CIDR block for Internet Gateway."
   type        = string
   default     = null
 }
@@ -33,7 +34,13 @@ variable "route_nat_gateway_bool" {
 }
 
 variable "route_nat_gateway" {
-  description = "The NAT Gateway ID."
+  description = "The NAT Gateway ID. (Use only if route_nat_gateway is TRUE.)"
+  type        = string
+  default     = null
+}
+
+variable "route_nat_gateway_destination_cidr_block" {
+  description = "The destination CIDR block for NAT Gateway."
   type        = string
   default     = null
 }
@@ -45,7 +52,13 @@ variable "route_endpoint_bool" {
 }
 
 variable "route_endpoint" {
-  description = "The VPC Endpoint ID."
+  description = "The VPC Endpoint ID. (Use only if route_endpoint is TRUE.)"
+  type        = string
+  default     = null
+}
+
+variable "route_endpoint_destination_cidr_block" {
+  description = "The destination CIDR block for Endpoint."
   type        = string
   default     = null
 }
@@ -57,7 +70,13 @@ variable "route_peering_bool" {
 }
 
 variable "route_peering" {
-  description = "The VPC Peering ID."
+  description = "The VPC Peering ID. (Use only if route_peering is TRUE.)"
+  type        = string
+  default     = null
+}
+
+variable "route_vpc_peering_destination_cidr_block" {
+  description = "The destination CIDR block for VPC Peering."
   type        = string
   default     = null
 }
