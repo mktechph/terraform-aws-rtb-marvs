77ab81bcbbabc2dde0dbaa87837e0f792dc5c24c
Marvin Balbutin <balbutinmarvin@gmail.com>
2024-10-30 17:38:15 +0800

    Add outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "outputs_rtb_id" {
   value = aws_route_table.rtb.id
 }
+
+output "outputs_route_route_endpoint" {
+  value = var.route_endpoint
+}
+
+output "outputs_route_route_nat_gateway" {
+  value = var.route_nat_gateway
+}
+
+output "outputs_route_route_internet_gateway" {
+  value = var.route_internet_gateway
+}
+
+output "outputs_route_route_peering" {
+  value = var.route_peering
+}
